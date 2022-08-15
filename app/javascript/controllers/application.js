69ccd0c4b92f797d3306dfbcd0faf3be16f226f9
github username <github email address>
2022-08-15 16:44:04 -0500

    Added navigation to authenticated views

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,14 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+// Import and register all TailwindCSS Components
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
+// application.register('alert', Alert)
+// application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+// application.register('modal', Modal)
+// application.register('tabs', Tabs)
+// application.register('popover', Popover)
+// application.register('toggle', Toggle)
+// application.register('slideover', Slideover)
