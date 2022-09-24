e2c239e7df2c9876b3f7f02fa6a01ac5f945faed
github username <github email address>
2022-09-24 10:09:07 -0500

    update pagination to use manual infinite scroll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :turbo_frame_request_variant
+
+  def turbo_frame_request_variant
+    request.variant = :turbo_frame if turbo_frame_request?
+  end
 end
